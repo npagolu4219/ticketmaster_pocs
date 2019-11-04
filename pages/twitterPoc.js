8c161b9acbb412878207d942c2c945bc6fee9627
Nagarjuna Pagolu <npagolu@aipl-lt-1195.local>
2019-11-04 11:21:43 +0530

    Added list in twitterpoc

--- pages/twitterPoc.js
+++ pages/twitterPoc.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import Layout from '../components/layout';
-import TwitterHash from '../components/twitter_hash';
-
+import TwitterHashDetails from '../components/hashtwitterData';
 class TwitterPOC extends React.Component {
   render() {
     return (
       <Layout>
         <br></br>
-        <TwitterHash />
+        <TwitterHashDetails />
       </Layout>
     )
   }
